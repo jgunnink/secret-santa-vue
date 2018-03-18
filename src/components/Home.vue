dbd0868cd1afe268899f0411ba6187b213b501ed
JK Gunnink <jgunnink@gmail.com>
2018-03-18 14:31:36 +0800

    Configure app with typescript and get homepage loading

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <br/>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Home",
+  data() {
+    return {
+      msg: "Welcome to Secret Santa",
+    };
+  },
+};
+</script>
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import App from "./App.vue";
+// import store from "./store";
+import router from "./router";
+
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+const v = new Vue({
+  el: "#app",
+  router,
+  //   store,
+  template: "<App/>",
+  components: { App },
+  data: { message: "this message is from the root vue" },
+});
