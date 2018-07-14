b74eb365b88a6c50ad1697a3970b5e80209199d8
JK Gunnink <jgunnink@gmail.com>
2018-07-14 21:38:18 +0800

    Update list page to insert santa details per santa. Also a filter

--- src/components/ListEditPage.vue
+++ src/components/ListEditPage.vue
@@ -3,32 +3,54 @@
     <h2>List Page!</h2>
     <p>{{this.$store.state.organiserName}}, let's organise your names for your {{this.$store.state.listName}} list.</p>
     <b-form inline @submit="onSubmit">
-      <template v-for="santa in 15">
-        <b-form-group id="santaGroup" v-bind:key="santa" style="margin-right: 20px">
-          <p>Santa {{santa}}</p>
-          <b-input id="nameInput" placeholder="John Smith" />
+      <template v-for="santa in santas">
+        <b-form-group v-bind:key="santas.indexOf(santa)" style="margin-right: 20px">
+          <p>Santa {{santas.indexOf(santa) + 1}}</p>
+          <b-input id="nameInput" required placeholder="John Smith" v-model="santa.name" />
           <div style="padding-bottom: 5px" />
-          <b-input id="emailInput" type="email" required placeholder="name@example.com" />
-          <div style="padding-bottom: 5px" />
-          <br>
+          <b-input id="emailInput" type="email" required placeholder="name@example.com" v-model="santa.email" />
+          <div style="margin-bottom: 20px" />
         </b-form-group>
       </template>
     </b-form>
-    <b-button type="submit" variant="primary">Submit</b-button>
+    <b-btn variant="primary" v-on:click="onSubmit">Shuffle and send emails</b-btn>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import { Santa } from "../types/index";
 
 @Component
 export default class ListEditPage extends Vue {
+  // 15 Empty Santas as generated on page load
+  santas: Santa[] = [
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" },
+    { name: "", email: "" }
+];
+
   onSubmit(evt) {
     evt.preventDefault();
+    const updatedList = this.filterEmptyRecords(this.santas);
+    console.log(updatedList)
+  }
+
+  filterEmptyRecords(santas) {
+    return santas.filter(santa => (santa.name != "") || (santa.email != ""));
   }
 }
 </script>
-
-<style scoped>
-</style>
